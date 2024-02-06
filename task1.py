07e19742a7a4239a33d5c267c7e15d2bb8919725
Shokhrukh Tukhtasinov <shoxruxbekbest@gmail.com>
2024-02-06 21:31:50 +0100

    new4

--- task1.py
+++ task1.py
@@ -1,15 +1,24 @@
 class User:
-    def __init__(self, id, username):
+    def __init__(self, id, username,):
         self.id = id
         self.username = username
-    
+        self.followers = 0
+        self.following = 0
 
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
 
 
 user1 = User("007", "Hacker")
+user2 = User("008", "Hacker2")
 
 # user1 = User()
 # user1.id = "007"
 # user1.name = "Hacker"
 
-print(user1.id)
+user1.follow(user2)
+
+print(user1.following)
+
+print(user2.followers)
