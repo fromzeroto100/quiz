100a398dfe735973d1af5cce60afff181498c633
Shokhrukh Tukhtasinov <shoxruxbekbest@gmail.com>
2024-02-06 21:12:22 +0100

    new2

--- task1.py
+++ task1.py
@@ -1,8 +1,15 @@
 class User:
-    pass
+    def __init__(self, id, username):
+        self.id = id
+        self.username = username
+    
 
-user1 = User()
-user1.id = "007"
-user1.name = "Hacker"
 
-print(user1.name)
+
+user1 = User("007", "Hacker")
+
+# user1 = User()
+# user1.id = "007"
+# user1.name = "Hacker"
+
+print(user1.id)
