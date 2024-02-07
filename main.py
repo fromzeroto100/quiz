57594b5edee353cae71818026c7ad8f6628da589
Shokhrukh Tukhtasinov <shoxruxbekbest@gmail.com>
2024-02-07 19:55:11 +0100

    new3

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ from question_model import Question
 from data import question_data
 
 
-question_bank = question_data
+question_bank = []
 
-for item in question_data(question_bank):
-    
+for question in question_data:
+    question_text = question["text"]
+    question_answer = question["answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+print(question_bank[0].text)
